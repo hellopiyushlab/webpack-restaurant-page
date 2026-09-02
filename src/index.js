0ec94c4e1c67711a6e17094fcc674c0c8c20b82c
piyush <hellopiyush.lab@gmail.com>
2026-09-02 19:12:05 +0000

    added classes and ids in buttons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import {createContent} from "./primary-dom-creation.js";
 import "./style.css";
-
-
+console.log("Everything is working fine.");
 
 createContent();
-console.log("Everything is working fine.");
+// now the goal is to handle events and use different module accordingly
