89d6c33d7b58cb2b44fd87c994b40d1bfa6637e7
piyush <hellopiyush.lab@gmail.com>
2026-09-02 18:52:59 +0000

    homepage working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import {createContent} from "./primary-dom-creation.js";
 import "./style.css";
+createContent();
 console.log("Everything is working fine.");
---
+++ src/primary-dom-creation.js
@@ -0,0 +1,31 @@
+// <div id="cafe-name">Kagami café</div>
+// <div id="tagline">Sip the warmth of freshly roasted coffee, savor the quiet comfort of your morning, and let the rest of the world slow down.</div>
+// <div id="online-button">Order Online</div>
+
+function createContent() {
+
+    // select the main content
+    const content = document.querySelector("#content");
+
+    // create the heading with cafe name
+    const cafeName = document.createElement("div");
+    cafeName.textContent = "Kagami café";
+    cafeName.setAttribute("id", "cafe-name");
+    content.appendChild(cafeName);
+
+    // create the tagline
+    const tagline = document.createElement("div");
+    tagline.textContent = "Sip the warmth of freshly roasted coffee, savor the quiet comfort of your morning, and let the rest of the world slow down.";
+    tagline.setAttribute("id", "tagline");
+    content.appendChild(tagline);
+
+    // create the order online button
+    const orderOnlineButton = document.createElement("div");
+    orderOnlineButton.textContent = "Order Online";
+    orderOnlineButton.setAttribute("id", "online-button");
+    content.appendChild(orderOnlineButton);
+}
+
+export {
+    createContent,
+};
